6ddaa8f1ad35b53309c261a937b012e77cbecf85
newrirte <nirn24ru@gmail.com>
2021-10-01 18:15:44 +0700

    Add LeveledNpc module

---
+++ SkyrimPatcher/src/LeveledNpc.fs
@@ -0,0 +1,12 @@
+﻿namespace Patcher
+
+open Mutagen.Bethesda
+open Mutagen.Bethesda.Skyrim
+open Mutagen.Bethesda.Plugins.Cache
+open Mutagen.Bethesda.FormKeys.SkyrimSE
+
+[<RequireQualifiedAccess>]
+module LeveledNpc =
+
+  let a = 1
+
--- SkyrimPatcher/src/Main.fs
+++ SkyrimPatcher/src/Main.fs
@@ -6,15 +6,14 @@ let main _ =
 
   let loadOrder = LoadOrder.PriorityOrderFromEnv SkyrimRelease.SkyrimLE
   let cache = Cache.ToImmutableLinkCache loadOrder
+
+  let modName = "ArmorTest.esp"
+  System.IO.File.Delete(modName)
+  let newMod = Mods.CreateSkyrimMod modName SkyrimRelease.SkyrimLE
+
   let armorsRecord =
     Records.Armor.WinningOverrides false (Seq.rev loadOrder)
     |>Seq.filter (isNull >> not)
-
-  let modName = "ArmorTest.esp"
-
-  System.IO.File.Delete(modName)
-
-  let newMod = Mods.CreateSkyrimMod modName SkyrimRelease.SkyrimLE
 
   Patcher.Armor.processPatchArmorsAndShieldsWithTemplate armorsRecord cache newMod
   Patcher.Armor.processPatchArmorsWithoutTemplateNoShield armorsRecord newMod
